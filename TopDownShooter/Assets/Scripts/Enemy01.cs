d14987279d406ae1bc516efc5b8a3597eb62a561
Wasanthraj <71727878+Wasanthraj@users.noreply.github.com>
2020-10-17 12:37:11 +0530

    Enemy Random Spawning

---
+++ TopDownShooter/Assets/Scripts/Enemy01.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy01 : MonoBehaviour
+{
+    //Enemy movement speed
+    [SerializeField]
+    private float movementSpeed = 0.0f;
+
+    //Enemy Rigidbody2D
+    public Rigidbody2D myRigidbody2D;
+    //rotate speed
+    public float rotateSpeed;
+    //Target = Player
+    private Transform target;
+    //Player direction
+    private Vector3 targetDirection;
+    //rotate direction
+    Vector3  rotateDirection;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        //Assiging the Variable to Rigidbody2D
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+        //Assiging the transform of Player
+        target = GameObject.FindGameObjectWithTag ("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        RotateGameObject(target,rotateSpeed,1.0f);
+    }
+
+    private void Movement()
+    {
+        // move sprite towards the target location
+        transform.position = Vector2.MoveTowards(transform.position, target.position, movementSpeed * Time.deltaTime);
+    }
+    
+    private void RotateGameObject(Transform target, float RotationSpeed, float offset)
+    {
+        //get the direction of the other object from current object
+        Vector3 targetDirection = target.position - transform.position;
+        //get the angle from current direction facing to desired target
+        float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
+        //set the angle into a quaternion + sprite offset depending on initial sprite facing direction
+        Quaternion rotation = Quaternion.Euler(new Vector3(0, 0, angle + offset));
+        //Roatate current game object to face the target using a slerp function which adds some smoothing to the move
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, RotationSpeed * Time.deltaTime);
+    }
+
+}
---
+++ TopDownShooter/Assets/Scripts/EnemyGenerator.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+
+    public GameObject enemyPooler;
+    public ObjectPooler objectPooler;
+
+    //Enemy respawn time
+    [SerializeField]
+    private float nextSpawnTime;
+
+    private float[] randomNums;
+
+    // private Vector2 Position;
+    private float[] positionX;
+    private float[] positionY;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectPooler = enemyPooler.GetComponent<ObjectPooler>();
+
+        //Random spawning time for enemy
+        randomNums = new float[] { 3.0f, 4.0f, 5.0f, 6.0f, 4.0f, 5.0f, 6.0f };
+        nextSpawnTime = randomNums[Random.Range(1, 7)];
+
+        positionX = new float[] { -7.2f, -4.4f, 1.6f, 4.8f, 5.2f};
+        positionY = new float[] { 3.2f, -0.9f, -3.7f, 2.0f, -2.8f};
+
+
+        StartCoroutine("MyCoroutine");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Enemy Generator
+    private void EnemyGenerate()
+    {   
+        Vector2 position = new Vector2 ( positionX[Random.Range(0,5)], positionY[Random.Range(0,5)] );
+        //Getting a Enemy object from ObjectPooler and assiging it to "newEnemy"
+        GameObject newEnemy = (GameObject)objectPooler.GetPooledObject();
+        if (newEnemy != null)
+        {
+            //Assiging the position of the enemy as same as the generator
+            newEnemy.transform.position = position;
+            newEnemy.transform.rotation = Quaternion.identity;
+
+            //Enabling Gameobject
+            newEnemy.SetActive(true);
+        }
+        
+
+    }
+    //Coroutine function for generating enemy
+    IEnumerator MyCoroutine()
+    {
+        for (; ; )
+        {
+            EnemyGenerate();
+            yield return new WaitForSeconds(nextSpawnTime);
+        }
+    }
+}
