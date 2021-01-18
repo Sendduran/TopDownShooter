3db0d7f458d950e7b5d2ad6921b6005ef272523f
Sendduran <66428404+Sendduran@users.noreply.github.com>
2021-01-18 20:00:45 +0530

    Revert "Revert "Merge branch 'main' into Feature/EnemyHealth""
    This reverts commit 4a5754ee55b25ed101c1e5b81f3be0aa8ca11adf.

---
+++ TopDownShooter/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    private Transform playerPos;
+    void Awake()
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(playerPos.position.x, playerPos.position.y, transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -10.42f, 10.42f), Mathf.Clamp(transform.position.y, -5.85f, 5.85f), transform.position.z);
+    }
+}
